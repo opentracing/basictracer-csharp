73bf3e50752268a7079d4037cdbe6f9782c85b5d
Christian Weiss <c.weiss@c3-ls.com>
2016-10-15 15:11:48 +0200

    code from opentracing-csharp

---
+++ src/OpenTracing.BasicTracer/ISpanContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenTracing.BasicTracer
+{
+    public interface ISpanContextFactory
+    {
+        SpanContext CreateSpanContext(IList<Tuple<string, ISpanContext>> references);
+    }
+}
---
+++ src/OpenTracing.BasicTracer/LogData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OpenTracing.BasicTracer
+{
+    public struct LogData
+    {
+        public LogData(DateTime timestamp, string eventName, object payload)
+        {
+            Timestamp = timestamp;
+            EventName = eventName;
+            Payload = payload;
+        }
+
+        public DateTime Timestamp { get; private set; }
+        public string EventName { get; private set; }
+        public object Payload { get; private set; }
+    }
+}
